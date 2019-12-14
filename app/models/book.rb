e5391d14660d39e0b53260ea098e6ade24f01bfa
Becky Robran <rerobran@gmail.com>
2019-12-14 07:31:55 -0700

    Adds validations and associations to models

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,8 @@
 class Book < ApplicationRecord
+  validates_presence_of :title
+  validates_presence_of :pages
+  validates_presence_of :publication_year
+
+  has_many :book_authors
+  has_many :authors, through: :book_authors
 end
