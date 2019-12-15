9d741b290910bbae95fdc3506ff3a33b11da1d6c
Becky Robran <rerobran@gmail.com>
2019-12-14 19:22:55 -0700

    Adds author model method test

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -9,4 +9,17 @@ RSpec.describe Author, type: :model do
     it {should have_many :book_authors }
     it {should have_many(:books).through(:book_authors) }
   end
+
+  describe "methods" do
+    it "can calculate an ave number of book pages per author" do
+      sona= Author.create!(name: "Sona Charaipotra")
+      pretty_things = sona.books.create!(title: "Tiny Pretty Things",
+                            pages: 256,
+                            publication_year: "2010")
+      another_book = sona.books.create!(title: "Another Book",
+                            pages: 316,
+                            publication_year: "2000")
+      expect(sona.ave_page_count).to eql(286)
+    end
+  end
 end
